d2fd7c255fafe42dcba5571365379e4f6afe8b49
P J Borowiecki <piotr.borowiecki@yahoo.com>
2024-03-24 00:38:50 +0100

    Remove redundant import to fix a compilation error

--- src/main/java/com/pjborowiecki/springbank/balance/BalanceController.java
+++ src/main/java/com/pjborowiecki/springbank/balance/BalanceController.java
@@ -1,7 +1,5 @@
 package com.pjborowiecki.springbank.balance;
 
-package com.pjborowiecki.springbank.balance;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
