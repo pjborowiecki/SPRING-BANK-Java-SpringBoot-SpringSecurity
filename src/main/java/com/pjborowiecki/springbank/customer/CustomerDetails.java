484686e2f88ee4566a9625030455f6e55f187210
P J Borowiecki <piotr.borowiecki@yahoo.com>
2024-03-25 03:05:00 +0100

    Refactor CustomerDetails (UserDetailsService implementation)

--- src/main/java/com/pjborowiecki/springbank/customer/CustomerDetails.java
+++ src/main/java/com/pjborowiecki/springbank/customer/CustomerDetails.java
@@ -1,9 +1,6 @@
 package com.pjborowiecki.springbank.customer;
 
-import java.util.List;
-import java.util.ArrayList;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,19 +18,13 @@ public class CustomerDetails implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        String userName;
-        String password;
-        List<GrantedAuthority> authorities = new ArrayList<>();
-
         Customer customer = customerRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        userName = customer.getEmail();
-        password = customer.getPassword();
-        authorities.add(new SimpleGrantedAuthority(customer.getRole()));
-
-        return new User(userName, password, authorities);
+        return User.withUsername(customer.getEmail())
+                .password(customer.getPassword())
+                .authorities(new SimpleGrantedAuthority(customer.getRole()))
+                .build();
     }
 
 }
